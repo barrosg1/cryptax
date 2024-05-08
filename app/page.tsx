b486011083799f7cb0460acf8da0a997bda59160
Gabriel Barros <gabrielbarros@gabriels-mbp.mynetworksettings.com>
2024-05-08 10:48:08 -0400

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-between p-24">
+      <div>hello</div>
+      <div>hello</div>
+      <div>hello</div>
+    </main>
+  )
+}
